8be0afed94c6c3956a22ee878dc2e572ffbafb87
JOHHAL6352 <johhal6352@students.ecpi.edu>
2013-11-18 14:14:49 -0500

    FIX

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,39 @@
 
-	 // DOM ready
-	 $(function() {
-	   
-      // Create the dropdown base
-      $("<select />").appendTo("nav");
-      
-      // Create default option "Go to..."
-      $("<option />", {
-         "selected": "selected",
-         "value"   : "",
-         "text"    : ""
-      }).appendTo("nav select");
-      
-      // Populate dropdown with menu items
-      $("nav a").each(function() {
-       var el = $(this);
-       $("<option />", {
-           "value"   : el.attr("href"),
-           "text"    : el.text()
-       }).appendTo("nav select");
-      });
-      
-	   // To make dropdown actually work
-	   // To make more unobtrusive: http://css-tricks.com/4064-unobtrusive-page-changer/
-      $("nav select").change(function() {
-        window.location = $(this).find("option:selected").val();
-        });
-	 
-     });
+// DOM ready
+$(function() {
+ 	   
+    // Create the dropdown base
+    $("<div />").appendTo("nav");
+         
+     //Icon font
+    $("<i />", {
+        "class": "fa fa-list"
+    }).appendTo("nav div");
+         
+    // Create the dropdown base
+    $("<select />").appendTo("nav div");
+       
+    // Create default option "Go to..."
+    $("<option />", {
+        "selected": "selected",
+        "value"   : " ",
+        "text"    : "",
+    }).appendTo("nav div select");
+
+       
+    // Populate dropdown with menu items
+    $("nav a").each(function() {
+        var el = $(this);
+        $("<option />", {
+            "value"   : el.attr("href"),
+            "text"    : el.text()
+       }).appendTo("nav div select");
+    });
+       
+ 	   // To make dropdown actually work
+ 	   // To make more unobtrusive: http://css-tricks.com/4064-unobtrusive-page-changer/
+    $("nav div select").change(function() {
+         window.location = $(this).find("option:selected").val();
+    });
+    
+});
